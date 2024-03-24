f8dcd5803f7f5c13079b63766c074261b9e9a5fd
N1kos9 <onkl3p@gmail.com>
2024-03-24 18:49:49 +0200

    cors politics #2

--- app/(root)/cart/page.tsx
+++ app/(root)/cart/page.tsx
@@ -30,9 +30,8 @@ const Cart = () => {
       if (!user) {
         router.push("sign-in");
       } else {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}checkout`, {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}checkout/`, {
           method: "POST",
-          mode: "no-cors",
           body: JSON.stringify({ cartItems: cart.cartItems, customer }),
         });
         const data = await res.json();
